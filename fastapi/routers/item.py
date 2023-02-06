da349c6f054f4f4563974e4fed9bad46b2373c8f
karnse <suphawit.se@ku.th>
2023-02-06 19:51:40 +0700

    fastapi with mongodb update

--- fastapi/routers/item.py
+++ fastapi/routers/item.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from typing import Union
+from typing import Union,Optional
 from pydantic import BaseModel
+from config.database import *
 
+db['errollment']
 class ItemDetail(BaseModel):
     item_color:str
     item_detail:str
@@ -13,7 +15,18 @@ class Item(BaseModel):
     item_detail:ItemDetail
 router = APIRouter(prefix='/items')
 
-
+@router.get("/")
+def query_item(item_id:int=0,item_name:str= " ",item_bool:bool=None):
+    if(item_bool==None):
+        return {"item_id": item_id,"item_name":item_name}
+    return {"item_id": item_id,"item_name":item_name,"item_bool":item_bool}
+@router.get("/std/")
+def query_student(std_id:int):
+    list_data= []
+    for i in db['enrrolment'].find({'std_id':std_id},{'_id':0}):
+        print("dsfgsdfigjknsdifgjosdifg",i)
+        list_data.append(i)
+    return {'data':list_data}
 @router.post('/1/foo/true')
 def show_item():
     return {"msg":"order"}
@@ -24,11 +37,7 @@ def create_item():
 @router.post('/{item_id}/{item_name}/{item_bool}')
 def show_item(item_id:Union[int,str],item_name:str,item_bool:bool):
     return {"item_id": item_id,"item_name":item_name,"item_bool":item_bool}
-@router.get("/")
-def query_item(item_id:int=0,item_name:str= " ",item_bool:bool|None=None):
-    if(item_bool==None):
-        return {"item_id": item_id,"item_name":item_name}
-    return {"item_id": item_id,"item_name":item_name,"item_bool":item_bool}
+
 
 @router.get('/with_body')
 def show_item_body(Item:Item, detail:ItemDetail):
